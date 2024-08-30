31bcd158eb8abec0f3ad2c96b25196998f7850b8
andreawickman <andrea.wickman23@imperial.ac.uk>
2024-08-30 12:56:23 +0100

    Delete src/derivatives.py

--- src/derivatives.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def first_derivative(df, x, y):
-    return np.gradient(df[y], df[x])
-
-def second_derivative(df, x, y):
-    first_deriv = np.gradient(df[y], df[x])
-    return np.gradient(first_deriv, df[x])
